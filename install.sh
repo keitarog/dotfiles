6a35d37bd1dbaa771f404f90b8723c0503ec6e8d
ogukei <ogukei@ogukei.local>
2017-04-04 19:24:21 +0900

    First commit

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DOTPATH=~/.dotfiles
+GITHUB_URL="https://github.com/keitarog/dotfiles/archive/master.tar.gz"
+
+echo WELCOME!
+
+# dowload and extract tarball
+curl $GITHUB_URL | tar xv -
+mv -f dotfiles-master "$DOTPATH"
+
+# cd to dotpath
+cd $DOTPATH
+if [ $? -ne 0 ]; then
+	die "not found: $DOTPATH"
+fi
+
+# linkage
+for f in .??*
+do
+    [ "$f" = ".git" ] && continue
+    ln -snfv "$DOTPATH/$f" "$HOME/$f"
+done
+
