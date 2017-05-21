2360003982b47caa339cdd9a830d6996e0d67184
keitarog <keitarog@users.noreply.github.com>
2017-05-21 11:53:40 +0900

    Update

--- macos/init/src1_link.sh
+++ macos/init/src1_link.sh
@@ -1,3 +1,6 @@
 
-echo "source ~/.dotfiles/etc/macos/.bash_profile" >> ~/.bash_profile
+LINE="source ~/.dotfiles/etc/macos/.bash_profile"
+FILE="~/.bash_profile"
+
+grep -qF "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
